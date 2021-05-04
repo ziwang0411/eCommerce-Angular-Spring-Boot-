76585acac826a8157af5807fd45ef5775de40982
Zi Wang <Gary@oumatoMacBook-Pro.local>
2021-05-03 19:54:23 -0700

    added frontend

---
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+import {map} from 'rxjs/operators'
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8080/api/products';
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(theCategoryId:number): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
+}
